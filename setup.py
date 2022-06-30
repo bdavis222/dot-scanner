f65d2378fd005d0b3cf62ed5367ce972f7303371
bdavis222 <24298180+bdavis222@users.noreply.github.com>
2022-06-30 01:05:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="Dot Scanner",  # Required
-    version="0.1.0",  # Required
+    version="1.0.0",  # Required
     description="A program designed for analysis of microscope imaging data",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional
