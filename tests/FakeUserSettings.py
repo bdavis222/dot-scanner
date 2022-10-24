4405f79c3df22ad1ff55bf1a7064db503d45cf90
bdavis222 <24298180+bdavis222@users.noreply.github.com>
2022-10-24 10:07:45 +0100

    Update FakeUserSettings.py

--- tests/FakeUserSettings.py
+++ tests/FakeUserSettings.py
@@ -1,10 +1,12 @@
 class FakeUserSettings:
-    def __init__(self, dotSize=2, blobSize=5, skipsAllowed=0, thresholds=(1.5, 5.0, 2.0), 
-                    removeEdgeFrames=True):
-        self.filepath = "test/directory/"
-        self.startImage = ""
+    def __init__(self, filepath="test/directory/", dotSize=2, blobSize=5, saveFigures=False, 
+                    startImage="", skipsAllowed=0, removeEdgeFrames=True, 
+                    thresholds=(1.5, 5.0, 2.0)):
+        self.filepath = filepath
         self.dotSize = dotSize
         self.blobSize = blobSize
+        self.saveFigures = saveFigures
+        self.startImage = startImage
         self.skipsAllowed = skipsAllowed
-        self.thresholds = thresholds
         self.removeEdgeFrames = removeEdgeFrames
+        self.thresholds = thresholds
