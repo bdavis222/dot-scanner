4aae53a91646d57605f80111ed96a8191eb7e378
bdavis222 <24298180+bdavis222@users.noreply.github.com>
2022-10-25 10:44:34 +0100

    Update lifetime.py

--- dotscanner/lifetime.py
+++ dotscanner/lifetime.py
@@ -1,9 +1,9 @@
-import dotscanner.config as cfg
 import dotscanner.files as files
 import dotscanner.dataprocessing as dp
 import dotscanner.strings as strings
 import dotscanner.ui.window as ui
 from dotscanner.ui.MicroscopeImage import MicroscopeImage
+import settings.config as cfg
 import matplotlib.pyplot as pl
 import numpy as np
 import os
