a163fc550ab2505c7c22c1f1b4a34fa96b740e8d
bdavis222 <24298180+bdavis222@users.noreply.github.com>
2022-10-26 18:28:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="dotscanner",  # Required
+    name="Dot Scanner",  # Required
     version="1.2.0",  # Required
     description="A program designed for analysis of microscope imaging data",  # Optional
     long_description=long_description,  # Optional
