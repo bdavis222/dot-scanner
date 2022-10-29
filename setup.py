439d754ebac6ecaa64810107bee96c6f7630f4d7
bdavis222 <24298180+bdavis222@users.noreply.github.com>
2022-10-29 15:53:03 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="dotscanner",  # Required
-    version="1.2.8",  # Required
-    description="A program designed for analysis of microscope imaging data",  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type="text/markdown",  # Optional
-    url="https://github.com/bdavis222/dotscanner",  # Optional
-    author="Holly Allen and Brian Davis",  # Optional
-    author_email="holly.allen@colorado.edu",  # Optional
-    classifiers=[  # Optional
+    name="dotscanner",
+    version="1.2.8",
+    description="A program designed for analysis of microscope imaging data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bdavis222/dotscanner",
+    author="Holly Allen and Brian Davis",
+    author_email="holly.allen@colorado.edu",
+    classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
@@ -23,12 +23,11 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows"
     ],
-    # packages=["dotscanner", "tests"],  # Required
     packages=find_packages(),
     py_modules=["dotscanner", "dotscanner.ui", "settings", "tests"],
     python_requires=">=3.7, <4",
     install_requires=["matplotlib", "numpy"],
-    project_urls={  # Optional
+    project_urls={
         "Bug Reports": "https://github.com/bdavis222/dotscanner/issues",
         "Funding": "https://www.paypal.com/donate/?business=UA5NL9MJSFMVY",
         "Source": "https://github.com/bdavis222/dotscanner",
